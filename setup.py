f7d67dc6ecbb90eb2cf72799efcbf3b48acf46fa
matthewkirby <matthewkirby@email.arizona.edu>
2017-04-25 00:33:29 -0700

    Released on pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='samplePKG',
-      version='0.1dev',
+      version='1.0',
       description='Performs basic math functions',
       author='Matthew Kirby',
       author_email='matthewkirby@email.arizona.edu',
