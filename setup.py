a19a0ec66bc80d29c94fefbc94666967a0953820
matthewkirby <matthewkirby@email.arizona.edu>
2017-04-25 00:10:20 -0700

    Made a setup.py file and constructed a package.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='samplePKG',
+      version='0.1dev',
+      description='Performs basic math functions',
+      author='Matthew Kirby',
+      author_email='matthewkirby@email.arizona.edu',
+      packages=['samplePKG'],
+     )
