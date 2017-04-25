039a169e2fa31fc580861b19612d65b5449b0cd6
matthewkirby <matthewkirby@email.arizona.edu>
2017-04-25 00:56:10 -0700

    Fixed broken assert

--- samplePKG/tests/test_module1.py
+++ samplePKG/tests/test_module1.py
@@ -3,6 +3,6 @@ import samplePKG as s
 def testAdd():
     assert s.myAdd(1,2)==3
 def breakAdd():
-    assert s.myAdd(1,2)==22
+    assert s.myAdd(5,6)==11
 def testSub():
     assert s.mySub(2,1)==1
