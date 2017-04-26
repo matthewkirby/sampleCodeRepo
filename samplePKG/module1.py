6ece3c02dfba3db47b59d42023ff7a506c661369
matthewkirby <matthewkirby@email.arizona.edu>
2017-04-25 17:12:11 -0700

    Added a docstring to myAdd function

--- samplePKG/module1.py
+++ samplePKG/module1.py
@@ -1,4 +1,19 @@
 def myAdd(a, b):
+    """
+    Adds two numbers together
+
+    Parameters
+    ----------
+    a : int or float
+        First of two numbers to add together
+    b : int or float
+        Second of two numbers to add together
+
+    Returns
+    -------
+    int or float
+        Returns the sum of the two parameters
+    """
     return a+b
 
 def mySub(a, b):
